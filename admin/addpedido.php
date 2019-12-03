0f06191389405351a85c671561d3c8e31c49ed33
Said <said.g@hotmail.com>
2019-12-03 13:34:28 -0600

    Bladis

--- admin/addpedido.php
+++ admin/addpedido.php
@@ -5,7 +5,7 @@
     }
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD','1234');
+    define('DB_PASSWORD','');
     define('DB_DATABASE', 'sistema_web');
     $conexion = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     if ($conexion == false) 
