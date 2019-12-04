205cc8da499a345007d4af3ce765526ade15c009
Said <said.g@hotmail.com>
2019-12-04 13:53:25 -0600

    tabla

--- cliente/cuenta.php
+++ cliente/cuenta.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['id'])) {
   <title>Cuenta</title>
   <link rel="stylesheet" href="../admin/dash.css">
   <style>
-    body{
+    body {
       font-size: 15pt;
       color: white;
       font-family: poppins, sans-serif;
@@ -26,50 +26,74 @@ if (!isset($_SESSION['id'])) {
 <body>
   <h1>Bienvenido a tu cuenta</h1>
   <center>
-  <?php
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Precio Unitario</th>
+        <th>Monto total</th>
+      </tr>
+      <tr>
+        <td>
+          <?php
 
-  $sql = "SELECT * FROM pedidos where id_cliente = ? AND estado_pedido = 'activo'";
-  $stmt = $conn->prepare($sql);
-  $stmt->execute(array($_SESSION['id']));
-  $pedidos = $stmt->fetchAll();
-  foreach ($pedidos as $pedido) {
-    ?>
-    <h3>Pedido</h3>
+          $sql = "SELECT * FROM pedidos where id_cliente = ? AND estado_pedido = 'activo'";
+          $stmt = $conn->prepare($sql);
+          $stmt->execute(array($_SESSION['id']));
+          $pedidos = $stmt->fetchAll();
+          if ($pedidos) {
+            foreach ($pedidos as $pedido) {
+              ?>
+              <?php
+                  echo $pedido['id_pedido'];
+                  ?>
+        </td>
+        <td>
+          <?php
+              echo $pedido['precio_unitario'];
+              ?>
+        </td>
+        <td>
+          <?php
+              echo $pedido['monto_total'];
+              ?>
+        </td>
+      </tr>
+    </table>
     <?php
-    // echo $pedido['id_pedido'];
-    echo " Precio unitario ";
-    echo $pedido['precio_unitario'];
-    echo " Precio Total ";
-    echo $pedido['monto_total'];
-    echo "<br>";
 
-    $sql = "SELECT monto_pago, fecha_pago FROM pagos where id_pedido = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(array($pedido['id_pedido']));
-    $pagos = $stmt->fetchAll();
-    $monto = 0;
-    foreach($pagos as $pago){
-      ?>
-    <h4>pagos</h4>
+        $sql = "SELECT monto_pago, fecha_pago FROM pagos where id_pedido = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array($pedido['id_pedido']));
+        $pagos = $stmt->fetchAll();
+        $monto = 0;
+        foreach ($pagos as $pago) {
+          ?>
+      <h4>pagos</h4>
     <?php
-      echo $pago['fecha_pago'];
-      echo " ";
-      echo $pago['monto_pago'];
-      echo " ";
-      $monto += $pago['monto_pago'];
-      echo '<br>';
-    }
-    ?>
-    <h5>Restan: 
-    <?php
-    echo $pedido['monto_total'] - $monto;
-    ?>
+          echo $pago['fecha_pago'];
+          echo " ";
+          echo $pago['monto_pago'];
+          echo " ";
+          $monto += $pago['monto_pago'];
+          echo '<br>';
+        }
+        ?>
+    <h5>Restan: $
+      <?php
+          echo $pedido['monto_total'] - $monto;
+          ?>
     </h5>
-    <?php
-  }
-  
-  
-  ?>
+  <?php
+    }
+  } else {
+    ?>
+  <h5>No tienes pedidos</h5>
+<?php
+}
+
+
+?>
+
   </center>
 </body>
 
