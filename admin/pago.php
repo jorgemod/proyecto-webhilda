d7c6bf44e34b1e44c378dce6144ce170ed896b28
Said <said.g@hotmail.com>
2019-12-04 11:28:36 -0600

    ya quedo

--- admin/pago.php
+++ admin/pago.php
@@ -21,7 +21,7 @@ if ($clientes->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="dash.css">
-    <title>Inicio</title>
+    <title>Pago</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
     <script>
